376d017e41cb9fbbdd1ab43cafaf61325d6bfa57
victor-jaimes-puente <victor.jaimes.puente@gmail.com>
2020-03-25 12:42:31 -0500

    updating tree after file corrections

---
+++ TreeHouse/src/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {
+    public static void main(String[] arg){
+        printThis();
+    }
+    public static void printThis(){
+        System.out.println();
+        System.out.println("Hello");
+    }
+}
